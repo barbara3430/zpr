46ac19ea5a7c8c322e2e4d013b7de149a28bae73
Wiktor <wikii122@gmail.com>
2014-01-02 23:08:39 +0100

    Replaced CGI with FCGI

---
+++ fcgi/hello.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python2
+import cgi
+import string
+import userbase
+
+def myapp(environ, start_response):
+        print('got request: %s' % environ)
+        start_response('200 OK', [('Content-Type', 'text/plain')])
+        # Create instance of FieldStorage
+        form = cgi.FieldStorage(
+                fp=environ['wsgi.input'],
+                environ=environ,
+                keep_blank_values=True)
+
+        # Get data from fields
+        first_name = form.getvalue('first_name')
+        last_name  = form.getvalue('last_name')
+
+        # get data from c++ user base
+        userbase.addToBase(first_name)
+        users = userbase.showBase()
+
+        #read html of main page
+        f = open('main.html')
+        mainhtml = string.Template( f.read() )
+        f.close()
+
+        #fill main page with user data
+        result = mainhtml.safe_substitute(name=first_name, users=users)
+
+        print("Content-type:text/html\r\n\r\n")
+        return result
+
+if __name__ == '__main__':
+    from flup.server.fcgi import WSGIServer
+    WSGIServer(myapp).run()
