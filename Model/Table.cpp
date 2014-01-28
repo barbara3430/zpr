9a5e10cfd72e67fcd07198a74b34b8a20691041d
kasiakw <kasia.kw04@gmail.com>
2014-01-28 20:32:13 +0100

    return string not JSON

--- Model/Table.cpp
+++ Model/Table.cpp
@@ -131,7 +131,7 @@ void Table::activate_player(unsigned s)
 	}
 }
 
-Json::Value Table::playerRaise(unsigned s,unsigned raise) //przebicie
+std::string  Table::playerRaise(unsigned s,unsigned raise) //przebicie
 {
 	unsigned ind = 2;
 	for(unsigned i=0; i<MAX_SEATS; ++i){
@@ -162,7 +162,7 @@ Json::Value Table::playerRaise(unsigned s,unsigned raise) //przebicie
 	return renderPlayerJson(s);
 }
 
-Json::Value Table::playerCheck(unsigned s) //sprawdzenie
+std::string  Table::playerCheck(unsigned s) //sprawdzenie
 {
 	unsigned ind = 2;
 	for(unsigned i=0; i<MAX_SEATS; ++i){
@@ -198,7 +198,7 @@ Json::Value Table::playerCheck(unsigned s) //sprawdzenie
 	return renderPlayerJson(s);
 }
 
-Json::Value Table::playerFold(unsigned s)
+std::string  Table::playerFold(unsigned s)
 {
 	unsigned ind = 2;
 	for(unsigned i=0; i<MAX_SEATS; ++i){
@@ -225,7 +225,7 @@ Json::Value Table::playerFold(unsigned s)
 	return renderPlayerJson(s);
 }
 
-Json::Value Table::playerAllIn(unsigned s)
+std::string  Table::playerAllIn(unsigned s)
 {
 	unsigned ind = 2;
 	for(unsigned i=0; i<MAX_SEATS; ++i){
@@ -390,7 +390,7 @@ void Table::getWinners()
 	}
 }
 
-Json::Value Table::addPlayer(std::string n)
+std::string Table::addPlayer(std::string n)
 {
     if(players.size() < MAX_SEATS) {
       unsigned s;
@@ -406,7 +406,7 @@ Json::Value Table::addPlayer(std::string n)
     return getJsonError("addPlayerError", "There are already two players");
 }
 
-Json::Value Table::playerChange(unsigned s, boost::python::list& ns){
+std::string  Table::playerChange(unsigned s, boost::python::list& ns){
   
   	std::vector<unsigned> c;	
         for (int i = 0; i < len(ns); ++i)
@@ -479,7 +479,7 @@ Json::Value Table::playerChange(unsigned s, boost::python::list& ns){
 	return setCardsJson(s);
 }
 
-Json::Value Table::updateNames(unsigned s)
+std::string  Table::updateNames(unsigned s)
 {
   	unsigned ind = 2;
 	for(unsigned i=0; i<MAX_SEATS; ++i){
@@ -509,7 +509,7 @@ Json::Value Table::updateNames(unsigned s)
   
 }
 
-Json::Value Table::updateGame(unsigned s)
+std::string  Table::updateGame(unsigned s)
 {
     	unsigned ind = 2;
 	for(unsigned i=0; i<MAX_SEATS; ++i){
@@ -547,23 +547,27 @@ Json::Value Table::updateGame(unsigned s)
 	}
 }
 
-Json::Value Table::getJsonError(std::string name, std::string info)
+std::string Table::getJsonError(std::string name, std::string info)
 {
 	Json::Value value(Json::objectValue);
 	value["name"] = name;
 	value["error"] = info;
-	return value;
+	
+	Json::StyledWriter writer;
+	return writer.write( value );
 }
 
-Json::Value Table::addPlayerJson(unsigned s)
+std::string  Table::addPlayerJson(unsigned s)
 {
 	Json::Value value(Json::objectValue);
 	value["method"] = "loginSuccess";
 	value["parameters"] = s;
-	return value;
+	
+	Json::StyledWriter writer;
+	return writer.write( value );
 }
 
-Json::Value Table::refreshNamesJson(unsigned s)
+std::string Table::refreshNamesJson(unsigned s)
 {
     	unsigned ind = 2;
 	for(unsigned i=0; i<MAX_SEATS; ++i){
@@ -578,10 +582,12 @@ Json::Value Table::refreshNamesJson(unsigned s)
 	Json::Value value(Json::objectValue);
 	value["method"] = "refreshNames";
 	value["parameters"]["name"] = players[ind].name;
-	return value;
+	
+	Json::StyledWriter writer;
+	return writer.write( value );
 }
  //[{"username":nazwa,"method":"startGame":{player: {account: int, bid: int, name: str, cards: [int, int, int, int, int]}, others: [{account: int, bid: int, name: str}]}}]
-Json::Value Table::startGameJson(unsigned s)
+std::string  Table::startGameJson(unsigned s)
 {
   	unsigned ind = 2;
 	for(unsigned i=0; i<MAX_SEATS; ++i){
@@ -613,11 +619,13 @@ Json::Value Table::startGameJson(unsigned s)
 	value["parameters"]["others"]["account"] = players[ind].cash_available;
 	value["parameters"]["others"]["bid"] = players[ind].bet_this_turn;
 	value["parameters"]["others"]["name"] = players[ind].name;
-	return value;
+	
+	Json::StyledWriter writer;
+	return writer.write( value );
 }
 
 // [{"username":nazwa,"method":"refreshState",{player: {account: int, bid: int, name: str, state: int(0-2)}, others: [{account: int, bid: int, name: str}]}}] 
-Json::Value Table::refreshStateJson(unsigned s, int st)
+std::string  Table::refreshStateJson(unsigned s, int st)
 {
     	unsigned ind = 2;
 	for(unsigned i=0; i<MAX_SEATS; ++i){
@@ -643,11 +651,13 @@ Json::Value Table::refreshStateJson(unsigned s, int st)
 	value["parameters"]["others"]["account"] = players[ind].cash_available;
 	value["parameters"]["others"]["bid"] = players[ind].bet_this_turn;
 	value["parameters"]["others"]["name"] = players[ind].name;
-	return value; 
+
+	Json::StyledWriter writer;
+	return writer.write( value );
 }
 
 //[{"username":nazwa,"method":"finishGame", parameters: {'won':boolean}}]
-Json::Value Table::finishGameJson(unsigned s)
+std::string  Table::finishGameJson(unsigned s)
 {
       	unsigned ind = 2;
 	for(unsigned i=0; i<MAX_SEATS; ++i){
@@ -672,11 +682,13 @@ Json::Value Table::finishGameJson(unsigned s)
 	}
 	
 	value["parameters"]["won"] = isWinner;
-	return value;
+	
+	Json::StyledWriter writer;
+	return writer.write( value );
 }
 
 //z parametrami {bid:int, account: int}
-Json::Value Table::renderPlayerJson(unsigned s)
+std::string  Table::renderPlayerJson(unsigned s)
 {
       	unsigned ind = 2;
 	for(unsigned i=0; i<MAX_SEATS; ++i){
@@ -694,10 +706,13 @@ Json::Value Table::renderPlayerJson(unsigned s)
 	value["method"] = "renderPlayer";
 	value["parameters"]["bid"] = players[ind].bet_this_turn;
 	value["parameters"]["account"] = players[ind].cash_available;
+	
+	Json::StyledWriter writer;
+	return writer.write( value );
 }
 
 //method: setCards, parameters {[lista wszystkich kart gracza}]
-Json::Value Table::setCardsJson(unsigned s)
+std::string  Table::setCardsJson(unsigned s)
 {
     	unsigned ind = 2;
 	for(unsigned i=0; i<MAX_SEATS; ++i){
@@ -721,6 +736,9 @@ Json::Value Table::setCardsJson(unsigned s)
 	  array.append(v[i]);
 	}
 	value["parameters"]["cards"] = array;
+	
+	Json::StyledWriter writer;
+	return writer.write( value );
 }
 
 
